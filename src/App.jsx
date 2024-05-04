d30c294a50b680b6825527031547fed8d8f22899
unknown <abdullahabirweb10@gmail.com>
2024-05-04 13:23:05 +0600

    services

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Services from "./Services/Services";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   );
 };
