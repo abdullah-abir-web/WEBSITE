cddf459ef00a73ac96aca8cbe3beeaa243eb2c9e
unknown <abdullahabirweb10@gmail.com>
2024-05-05 11:28:48 +0600

    first commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import "./Hero.css";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import hero from '../../assets/hero.svg'
 const Hero = () => {
   const gsapRef = useRef();
 
@@ -16,7 +17,7 @@ const Hero = () => {
   });
   return (
     <div id="home" className="hero">
-      {/* <img src="/public/hero.png" width={230} height={230} alt="" /> */}
+      <img src={hero} width={300} height={300} alt="" />
       <h1 ref={gsapRef}>
         <span ref={gsapRef}>I'm Abdullah Abir,</span> frontend developer based
         in BD.
